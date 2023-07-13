cbb3f65c7f76ff5d9ea591c77c86d8c388c70ae2
Aleks-Kotenko <1991kotenko@gmail.com>
2023-07-13 10:36:41 +0300

    close-menu-when-press-link

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
+    menuLinks: document.querySelectorAll('.mob-menu-item a')
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
@@ -11,4 +12,12 @@
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
   }
+
+  refs.menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      toggleMenu();
+    });
+  });
+  
 })();
+
